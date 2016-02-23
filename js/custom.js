1c4d39e8469984cee1fb6eb35b9addf5f2c47470
societyofhospitalmedicine <societyofhospitalmedicine@gmail.com>
2016-02-23 20:35:08 +0000

    added custom js file with image map for pie chart

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+/* prevent default behavior in image maping for links*/
+var areas = document.getElementsByTagName('area');
+                        for(var i = 0; i<areas.length; i++){
+                            areas[i].addEventListener("click", function(e){
+                                e.preventDefault();
+                            });
+                        };
